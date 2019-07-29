41c85637c9922362e9daeaa2c343b8be1ec8b128
myname21-meet <myname21@meet.mit.edu>
2019-07-29 11:28:17 -0400

    lil

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
+
+    return render_template(
+"index.html")
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
